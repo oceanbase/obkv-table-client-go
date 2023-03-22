f5ef45fd1abf1581c3f069d3b3508edb57535162
WeiXinChan <chenwx6728@163.com>
2023-03-22 14:40:46 +0800

    feat:add log model

--- go.mod
+++ go.mod
@@ -1,3 +1,13 @@
 module github.com/oceanbase/obkv-table-client-go
 
 go 1.20
+
+require (
+	github.com/go-sql-driver/mysql v1.7.0
+	go.uber.org/zap v1.24.0
+)
+
+require (
+	go.uber.org/atomic v1.7.0 // indirect
+	go.uber.org/multierr v1.6.0 // indirect
+)
--- log/log.go
+++ log/log.go
@@ -1,1 +1,184 @@
+/*
+	日志模块需要实现的功能：
+	1. 提供默认的logger->done
+	2. 封装Debug()/Info()/Error()等接口->done
+	3. 提供日志分级写文件，文件轮转功能->todo
+	4. 提供注册日志句柄功能->todo
+
+*/
+
 package log
+
+import (
+	"go.uber.org/zap"
+	"go.uber.org/zap/zapcore"
+	"io"
+	"os"
+	"sync"
+	"time"
+)
+
+var (
+	globalMutex         sync.Mutex
+	defaultGlobalLogger = NewLogger(os.Stderr, InfoLevel, WithCaller(true))
+)
+
+type Level zapcore.Level
+
+type Logger struct {
+	l     *zap.Logger // zap ensure that zap.Logger is safe for concurrent use
+	level Level
+}
+
+const (
+	InfoLevel   Level = Level(zap.InfoLevel)   // 0, default level
+	WarnLevel   Level = Level(zap.WarnLevel)   // 1
+	ErrorLevel  Level = Level(zap.ErrorLevel)  // 2
+	DPanicLevel Level = Level(zap.DPanicLevel) // 3, used in development log
+	// PanicLevel logs a message, then panics
+	PanicLevel Level = Level(zap.PanicLevel) // 4
+	// FatalLevel logs a message, then calls os.Exit(1).
+	FatalLevel Level = Level(zap.FatalLevel) // 5
+	DebugLevel Level = Level(zap.DebugLevel) // -1
+)
+
+var (
+	Skip        = zap.Skip
+	Binary      = zap.Binary
+	Bool        = zap.Bool
+	Boolp       = zap.Boolp
+	ByteString  = zap.ByteString
+	Complex128  = zap.Complex128
+	Complex128p = zap.Complex128p
+	Complex64   = zap.Complex64
+	Complex64p  = zap.Complex64p
+	Float64     = zap.Float64
+	Float64p    = zap.Float64p
+	Float32     = zap.Float32
+	Float32p    = zap.Float32p
+	Int         = zap.Int
+	Intp        = zap.Intp
+	Int64       = zap.Int64
+	Int64p      = zap.Int64p
+	Int32       = zap.Int32
+	Int32p      = zap.Int32p
+	Int16       = zap.Int16
+	Int16p      = zap.Int16p
+	Int8        = zap.Int8
+	Int8p       = zap.Int8p
+	String      = zap.String
+	Stringp     = zap.Stringp
+	Uint        = zap.Uint
+	Uintp       = zap.Uintp
+	Uint64      = zap.Uint64
+	Uint64p     = zap.Uint64p
+	Uint32      = zap.Uint32
+	Uint32p     = zap.Uint32p
+	Uint16      = zap.Uint16
+	Uint16p     = zap.Uint16p
+	Uint8       = zap.Uint8
+	Uint8p      = zap.Uint8p
+	Uintptr     = zap.Uintptr
+	Uintptrp    = zap.Uintptrp
+	Reflect     = zap.Reflect
+	Namespace   = zap.Namespace
+	Stringer    = zap.Stringer
+	Time        = zap.Time
+	Timep       = zap.Timep
+	Stack       = zap.Stack
+	StackSkip   = zap.StackSkip
+	Duration    = zap.Duration
+	Durationp   = zap.Durationp
+	Any         = zap.Any
+
+	Info   = defaultGlobalLogger.Info
+	Warn   = defaultGlobalLogger.Warn
+	Error  = defaultGlobalLogger.Error
+	DPanic = defaultGlobalLogger.DPanic
+	Panic  = defaultGlobalLogger.Panic
+	Fatal  = defaultGlobalLogger.Fatal
+	Debug  = defaultGlobalLogger.Debug
+)
+
+var (
+	WithCaller    = zap.WithCaller
+	AddStacktrace = zap.AddStacktrace
+	AddCallerSkip = zap.AddCallerSkip
+)
+
+func init() {
+	globalMutex.Lock()
+	defaultGlobalLogger = NewLogger(os.Stderr, InfoLevel, AddCallerSkip(1))
+	globalMutex.Unlock()
+}
+
+type Field = zap.Field
+
+func (l *Logger) Debug(msg string, fields ...Field) {
+	l.l.Debug(msg, fields...)
+}
+
+func (l *Logger) Info(msg string, fields ...Field) {
+	l.l.Info(msg, fields...)
+}
+
+func (l *Logger) Warn(msg string, fields ...Field) {
+	l.l.Warn(msg, fields...)
+}
+
+func (l *Logger) Error(msg string, fields ...Field) {
+	l.l.Error(msg, fields...)
+}
+
+func (l *Logger) DPanic(msg string, fields ...Field) {
+	l.l.DPanic(msg, fields...)
+}
+
+func (l *Logger) Panic(msg string, fields ...Field) {
+	l.l.Panic(msg, fields...)
+}
+
+func (l *Logger) Fatal(msg string, fields ...Field) {
+	l.l.Fatal(msg, fields...)
+}
+
+func (l *Logger) Sync() error {
+	return l.l.Sync()
+}
+
+func Sync() error {
+	if nil != defaultGlobalLogger {
+		return defaultGlobalLogger.Sync()
+	}
+	return nil
+}
+
+func ResetDefaultLogger(l *Logger) {
+	globalMutex.Lock()
+	defaultGlobalLogger = l
+	globalMutex.Unlock()
+}
+
+type Option = zap.Option
+
+func NewLogger(writer io.Writer, level Level, opts ...Option) *Logger {
+	if writer == nil {
+		panic("the writer is nil")
+	}
+	cfg := zap.NewProductionConfig()
+	cfg.EncoderConfig.EncodeTime = func(t time.Time, enc zapcore.PrimitiveArrayEncoder) {
+		enc.AppendString(t.Format("2006-01-02T15:04:05.000Z0700"))
+	}
+
+	core := zapcore.NewCore(
+		zapcore.NewJSONEncoder(cfg.EncoderConfig),
+		zapcore.AddSync(writer),
+		zapcore.Level(level),
+	)
+
+	logger := &Logger{
+		l:     zap.New(core, opts...),
+		level: level,
+	}
+	return logger
+}
