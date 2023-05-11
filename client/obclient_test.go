ad75b906b37c6972b36bf3b5489bbdadd90cd227
WeiXinChan <43193589+WeiXinChan@users.noreply.github.com>
2023-05-11 18:15:46 +0800

    add integration test (#25)
    Co-authored-by: 43193589+WeiXinChan <WeiXinChan@users.noreply.github.com>

--- client/obclient_test.go
+++ client/obclient_test.go
@@ -2,12 +2,10 @@ package client
 
 import (
 	"context"
-	"sync"
-	"testing"
-	"time"
-
 	"github.com/agiledragon/gomonkey/v2"
 	"github.com/stretchr/testify/assert"
+	"sync"
+	"testing"
 
 	"github.com/oceanbase/obkv-table-client-go/config"
 	"github.com/oceanbase/obkv-table-client-go/route"
@@ -159,102 +157,4 @@ func TestObTableParam_ToString(t *testing.T) {
 	assert.Equal(t, param.String(), "ObTableParam{table:nil, tableId:0, partitionId:0}")
 	param = ObTableParam{nil, 500023, 500012}
 	assert.Equal(t, param.String(), "ObTableParam{table:nil, tableId:500023, partitionId:500012}")
-}
-
-func TestInsert(t *testing.T) {
-	// CREATE TABLE test(c1 bigint(20), c2 bigint(20), primary key (c1)) PARTITION BY hash(c1) partitions 2;
-	const (
-		configUrl    = "..."
-		fullUserName = "..."
-		passWord     = ""
-		sysUserName  = "root"
-		sysPassWord  = "..."
-		tableName    = "test"
-	)
-
-	cfg := config.NewDefaultClientConfig()
-	cfg.OperationTimeOut = time.Duration(1000000) * time.Millisecond
-	cli, err := NewClient(configUrl, fullUserName, passWord, sysUserName, sysPassWord, cfg)
-	assert.Equal(t, nil, err)
-
-	err = cli.AddRowKey(tableName, []string{"c1"})
-	assert.Equal(t, nil, err)
-	rowKey := []*table.Column{table.NewColumn("c1", int64(1))}
-	mutateColumns := []*table.Column{table.NewColumn("c2", int64(1))}
-	affectRows, err := cli.Insert(
-		context.TODO(),
-		tableName,
-		rowKey,
-		mutateColumns,
-	)
-	assert.Equal(t, nil, err)
-	assert.EqualValues(t, 1, affectRows)
-}
-
-func TestGet(t *testing.T) {
-	const (
-		configUrl    = "..."
-		fullUserName = "..."
-		passWord     = ""
-		sysUserName  = "root"
-		sysPassWord  = ""
-		tableName    = "test"
-	)
-
-	cfg := config.NewDefaultClientConfig()
-	cli, err := NewClient(configUrl, fullUserName, passWord, sysUserName, sysPassWord, cfg)
-	assert.Equal(t, nil, err)
-
-	err = cli.AddRowKey(tableName, []string{"c1"})
-	assert.Equal(t, nil, err)
-	rowKey := []*table.Column{table.NewColumn("c1", int64(1))}
-	selectColumns := []string{"c1", "c2"}
-	m, err := cli.Get(
-		context.TODO(),
-		tableName,
-		rowKey,
-		selectColumns,
-	)
-	assert.Equal(t, nil, err)
-	assert.EqualValues(t, 1, m["c1"])
-	assert.EqualValues(t, 1, m["c2"])
-}
-
-func TestUpdate(t *testing.T) {
-	// CREATE TABLE test(c1 bigint(20), c2 bigint(20), primary key (c1)) PARTITION BY hash(c1) partitions 2;
-	const (
-		configUrl    = "..."
-		fullUserName = "..."
-		passWord     = ""
-		sysUserName  = "root"
-		sysPassWord  = "..."
-		tableName    = "test"
-	)
-
-	cfg := config.NewDefaultClientConfig()
-	cfg.OperationTimeOut = time.Duration(1000000) * time.Millisecond
-	cli, err := NewClient(configUrl, fullUserName, passWord, sysUserName, sysPassWord, cfg)
-	assert.Equal(t, nil, err)
-
-	err = cli.AddRowKey(tableName, []string{"c1"})
-	assert.Equal(t, nil, err)
-	rowKey := []*table.Column{table.NewColumn("c1", int64(1))}
-	mutateColumns := []*table.Column{table.NewColumn("c2", int64(2))}
-	affectRows, err := cli.Update(
-		context.TODO(),
-		tableName,
-		rowKey,
-		mutateColumns,
-	)
-	assert.EqualValues(t, 1, affectRows)
-	selectColumns := []string{"c1", "c2"}
-	m, err := cli.Get(
-		context.TODO(),
-		tableName,
-		rowKey,
-		selectColumns,
-	)
-	assert.Equal(t, nil, err)
-	assert.EqualValues(t, 1, m["c2"])
-	assert.EqualValues(t, 2, m["c2"])
 }
---
+++ test/single_opreation_test.go
@@ -0,0 +1,158 @@
+package test
+
+import (
+	"context"
+	"github.com/oceanbase/obkv-table-client-go/table"
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
+// create table statement
+const (
+	hashTable = "CREATE TABLE IF NOT EXISTS hashTable(`c1` bigint(20) NOT NULL, c2 bigint(20) NOT NULL, PRIMARY KEY (`c1`)) PARTITION BY HASH(c1) PARTITIONS 2;"
+)
+
+func TestInsert(t *testing.T) {
+	tableName := "hashTable"
+	cli := newClient()
+	createTable(hashTable)
+	defer func() {
+		dropTable(tableName)
+	}()
+
+	err := cli.AddRowKey(tableName, []string{"c1"})
+	assert.Equal(t, nil, err)
+
+	rowKey := []*table.Column{table.NewColumn("c1", int64(1))}
+	mutateColumns := []*table.Column{table.NewColumn("c2", int64(1))}
+	affectRows, err := cli.Insert(
+		context.TODO(),
+		tableName,
+		rowKey,
+		mutateColumns,
+	)
+	assert.Equal(t, nil, err)
+	assert.EqualValues(t, 1, affectRows)
+
+	selectColumns := []string{"c1", "c2"}
+	m, err := cli.Get(
+		context.TODO(),
+		tableName,
+		rowKey,
+		selectColumns,
+	)
+	assert.Equal(t, nil, err)
+	assert.EqualValues(t, 1, m["c1"])
+	assert.EqualValues(t, 1, m["c2"])
+}
+
+func TestUpdate(t *testing.T) {
+	tableName := "hashTable"
+	cli := newClient()
+	createTable(hashTable)
+	defer func() {
+		dropTable("hashTable")
+	}()
+
+	err := cli.AddRowKey("hashTable", []string{"c1"})
+	assert.Equal(t, nil, err)
+
+	rowKey := []*table.Column{table.NewColumn("c1", int64(1))}
+	mutateColumns := []*table.Column{table.NewColumn("c2", int64(1))}
+	affectRows, err := cli.Insert(
+		context.TODO(),
+		"hashTable",
+		rowKey,
+		mutateColumns,
+	)
+	assert.Equal(t, nil, err)
+	assert.EqualValues(t, 1, affectRows)
+
+	updateColumns := []*table.Column{table.NewColumn("c2", int64(2))}
+	affectRows, err = cli.Update(
+		context.TODO(),
+		tableName,
+		rowKey,
+		updateColumns,
+	)
+	assert.Equal(t, nil, err)
+	assert.EqualValues(t, 1, affectRows)
+
+	selectColumns := []string{"c1", "c2"}
+	m, err := cli.Get(
+		context.TODO(),
+		tableName,
+		rowKey,
+		selectColumns,
+	)
+	assert.Equal(t, nil, err)
+	assert.EqualValues(t, 1, m["c1"])
+	assert.EqualValues(t, 2, m["c2"])
+}
+
+func TestGet(t *testing.T) {
+	tableName := "hashTable"
+	cli := newClient()
+	createTable(hashTable)
+	defer func() {
+		dropTable(tableName)
+	}()
+
+	err := cli.AddRowKey(tableName, []string{"c1"})
+	assert.Equal(t, nil, err)
+
+	rowKey := []*table.Column{table.NewColumn("c1", int64(1))}
+	mutateColumns := []*table.Column{table.NewColumn("c2", int64(1))}
+	affectRows, err := cli.Insert(
+		context.TODO(),
+		tableName,
+		rowKey,
+		mutateColumns,
+	)
+	assert.Equal(t, nil, err)
+	assert.EqualValues(t, 1, affectRows)
+
+	selectColumns := []string{"c1", "c2"} // select c1, c2
+	m, err := cli.Get(
+		context.TODO(),
+		tableName,
+		rowKey,
+		selectColumns,
+	)
+	assert.Equal(t, nil, err)
+	assert.EqualValues(t, 1, m["c1"])
+	assert.EqualValues(t, 1, m["c2"])
+
+	selectColumns = []string{"c1"} // select c1
+	m, err = cli.Get(
+		context.TODO(),
+		tableName,
+		rowKey,
+		selectColumns,
+	)
+	assert.Equal(t, nil, err)
+	assert.EqualValues(t, 1, m["c1"])
+	assert.EqualValues(t, nil, m["c2"])
+
+	selectColumns = nil // default select all when selectColumns is nil
+	m, err = cli.Get(
+		context.TODO(),
+		tableName,
+		rowKey,
+		selectColumns,
+	)
+	assert.Equal(t, nil, err)
+	assert.EqualValues(t, 1, m["c1"])
+	assert.EqualValues(t, 1, m["c2"])
+
+	deleteTable(tableName)
+	m, err = cli.Get(
+		context.TODO(),
+		tableName,
+		rowKey,
+		selectColumns,
+	)
+	assert.Equal(t, nil, err)
+	assert.EqualValues(t, nil, m["c1"])
+	assert.EqualValues(t, nil, m["c2"])
+}
