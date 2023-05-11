8d910d0b26405034478c560f16870a51cc4ea6e9
ji.haodong <ji.haodong@hellogroup.com>
2023-05-11 11:21:23 +0800

    add update ut

--- client/obclient_test.go
+++ client/obclient_test.go
@@ -219,3 +219,42 @@ func TestGet(t *testing.T) {
 	assert.EqualValues(t, 1, m["c1"])
 	assert.EqualValues(t, 1, m["c2"])
 }
+
+func TestUpdate(t *testing.T) {
+	// CREATE TABLE test(c1 bigint(20), c2 bigint(20), primary key (c1)) PARTITION BY hash(c1) partitions 2;
+	const (
+		configUrl    = "..."
+		fullUserName = "..."
+		passWord     = ""
+		sysUserName  = "root"
+		sysPassWord  = "..."
+		tableName    = "test"
+	)
+
+	cfg := config.NewDefaultClientConfig()
+	cfg.OperationTimeOut = time.Duration(1000000) * time.Millisecond
+	cli, err := NewClient(configUrl, fullUserName, passWord, sysUserName, sysPassWord, cfg)
+	assert.Equal(t, nil, err)
+
+	err = cli.AddRowKey(tableName, []string{"c1"})
+	assert.Equal(t, nil, err)
+	rowKey := []*table.Column{table.NewColumn("c1", int64(1))}
+	mutateColumns := []*table.Column{table.NewColumn("c2", int64(2))}
+	affectRows, err := cli.Update(
+		context.TODO(),
+		tableName,
+		rowKey,
+		mutateColumns,
+	)
+	assert.EqualValues(t, 1, affectRows)
+	selectColumns := []string{"c1", "c2"}
+	m, err := cli.Get(
+		context.TODO(),
+		tableName,
+		rowKey,
+		selectColumns,
+	)
+	assert.Equal(t, nil, err)
+	assert.EqualValues(t, 1, m["c2"])
+	assert.EqualValues(t, 2, m["c2"])
+}
