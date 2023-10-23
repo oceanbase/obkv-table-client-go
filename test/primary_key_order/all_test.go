61147eddd2cb99921991b7e31d324a7a61c20c0c
43193589+WeiXinChan <WeiXinChan@users.noreply.github.com>
2023-10-23 09:43:30 +0800

    add primary key order tests

---
+++ test/primary_key_order/all_test.go
@@ -0,0 +1,55 @@
+/*-
+ * #%L
+ * OBKV Table Client Framework
+ * %%
+ * Copyright (C) 2021 OceanBase
+ * %%
+ * OBKV Table Client Framework is licensed under Mulan PSL v2.
+ * You can use this software according to the terms and conditions of the Mulan PSL v2.
+ * You may obtain a copy of Mulan PSL v2 at:
+ *          http://license.coscl.org.cn/MulanPSL2
+ * THIS SOFTWARE IS PROVIDED ON AN "AS IS" BASIS, WITHOUT WARRANTIES OF ANY KIND,
+ * EITHER EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO NON-INFRINGEMENT,
+ * MERCHANTABILITY OR FIT FOR A PARTICULAR PURPOSE.
+ * See the Mulan PSL v2 for more details.
+ * #L%
+ */
+
+package primary_key_order
+
+import (
+	"os"
+	"testing"
+
+	"github.com/oceanbase/obkv-table-client-go/client"
+	"github.com/oceanbase/obkv-table-client-go/test"
+)
+
+var cli client.Client
+
+func setup() {
+	cli = test.CreateClient()
+
+	test.CreateDB()
+
+	test.CreateTable(testPrimaryKeyTCreateStatement1)
+	test.CreateTable(testPrimaryKeyTCreateStatement2)
+	test.CreateTable(testPrimaryKeyTCreateStatement3)
+}
+
+func teardown() {
+	cli.Close()
+
+	test.DropTable(testPrimaryKeyTableName1)
+	test.DropTable(testPrimaryKeyTableName2)
+	test.DropTable(testPrimaryKeyTableName3)
+
+	test.CloseDB()
+}
+
+func TestMain(m *testing.M) {
+	setup()
+	code := m.Run()
+	teardown()
+	os.Exit(code)
+}
