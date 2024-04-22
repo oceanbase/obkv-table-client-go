fb1ad0cb110bbc1439ed168d9603efa863d90335
lyxiong0 <bear507@163.com>
2024-04-22 14:46:25 +0800

    [feat] add rpc server framework

--- go.mod
+++ go.mod
@@ -11,26 +11,28 @@ require (
 	github.com/pierrec/lz4/v4 v4.1.18
 	github.com/pkg/errors v0.9.1
 	github.com/scylladb/go-set v1.0.2
-	github.com/stretchr/testify v1.8.0
+	github.com/stretchr/testify v1.9.0
 	go.uber.org/zap v1.24.0
 	golang.org/x/crypto v0.17.0
 )
 
+require github.com/frankban/quicktest v1.14.6 // indirect
+
+require (
+	go.uber.org/goleak v1.3.0 // indirect
+	gopkg.in/check.v1 v1.0.0-20180628173108-788fd7840127 // indirect
+)
+
 require (
 	github.com/davecgh/go-spew v1.1.1 // indirect
-	github.com/fatih/color v1.16.0 // indirect
-	github.com/frankban/quicktest v1.14.6 // indirect
-	github.com/google/uuid v1.5.0 // indirect
-	github.com/jessevdk/go-flags v1.5.0 // indirect
+	github.com/google/uuid v1.6.0
 	github.com/kylelemons/godebug v1.1.0 // indirect
-	github.com/mattn/go-colorable v0.1.13 // indirect
-	github.com/mattn/go-isatty v0.0.20 // indirect
 	github.com/naoina/go-stringutil v0.1.0 // indirect
+	github.com/panjf2000/ants/v2 v2.9.1
 	github.com/pmezard/go-difflib v1.0.0 // indirect
-	github.com/uber/go-torch v0.0.0-20181107071353-86f327cc820e // indirect
 	go.uber.org/atomic v1.10.0 // indirect
 	go.uber.org/multierr v1.11.0 // indirect
-	golang.org/x/sys v0.15.0 // indirect
-	gopkg.in/natefinch/lumberjack.v2 v2.2.1 // indirect
+	golang.org/x/sys v0.15.0
+	gopkg.in/natefinch/lumberjack.v2 v2.2.1
 	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
---
+++ test/obkvrpc/rpc_server_test.go
@@ -0,0 +1,151 @@
+/*-
+ * #%L
+ * OBKV Table Client Framework
+ * %%
+ * Copyright (C) 2021 OceanBase
+ * %%
+ * OBKV Table Client Framework is licensed under Mulan PSL v2.
+ * You can use this software according to the terms and conditions of the Mulan PSL v2.
+ * You may obtain a copy of Mulan PSL v2 at:
+ *          http://license.coscl.org.cn/MulanPSL2
+ * THIS SOFTWARE IS PROVIDED ON AN "AS IS" BASIS, WITHOUT WARRANTIES OF ANY KIND,
+ * EITHER EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO NON-INFRINGEMENT,
+ * MERCHANTABILITY OR FIT FOR A PARTICULAR PURPOSE.
+ * See the Mulan PSL v2 for more details.
+ * #L%
+ */
+
+package obkvrpc
+
+import (
+	"fmt"
+	"io"
+	"net"
+	"sync"
+	"testing"
+	"time"
+
+	"github.com/google/uuid"
+	"github.com/oceanbase/obkv-table-client-go/obkvrpc"
+	"github.com/stretchr/testify/assert"
+)
+
+var (
+	MaxMsgNum       = 3
+	MaxClientNum    = 10
+	RoutinePoolSize = 10000
+	ExpiredDuration = time.Second * 60
+)
+
+type MockCodec struct {
+	Conn      net.Conn
+	RunTime   int
+	Wg        *sync.WaitGroup
+	IDMap     sync.Map
+	Test      *testing.T
+	CloseChan chan struct{}
+}
+
+func (mcs *MockCodec) GetCloseChan() *chan struct{} {
+	return &mcs.CloseChan
+}
+
+func (mcs *MockCodec) ReadRequest(req *obkvrpc.Request) error {
+	if mcs.RunTime++; mcs.RunTime > MaxMsgNum {
+		// fmt.Println("ReadRequest return err", mcs.RunTime)
+		return io.EOF
+	}
+	req.Method = "testMethod"
+	req.ID = uuid.New().String()
+
+	arg1 := []byte{'a', 'r', 'g'}
+	arg2 := []byte{'1', '2', '3'}
+	req.Args = append(req.Args, arg1)
+	req.Args = append(req.Args, arg2)
+	mcs.IDMap.Store(req.ID, true)
+	// fmt.Println("ReadRequest: ", req.ID, obkvrpc.GetGID())
+	return nil
+}
+
+func (mcs *MockCodec) WriteResponse(resp *obkvrpc.Response) error {
+	_, ok := mcs.IDMap.Load(resp.ID)
+	assert.Equal(mcs.Test, true, ok)
+	mcs.IDMap.Delete(resp.ID)
+	// fmt.Println("WriteResponse: ", resp.ID, obkvrpc.GetGID())
+	return nil
+}
+
+func (mcs *MockCodec) Call(req *obkvrpc.Request, resp *obkvrpc.Response) error {
+	resp.ID = req.ID
+	resp.RspContent = []byte("content")
+	// fmt.Println("Call: ", req.ID, obkvrpc.GetGID())
+	return nil
+}
+
+func (mcs *MockCodec) Close() {
+	// fmt.Println("MockCodec closed")
+	mcs.Conn.Close()
+	mcs.Wg.Done()
+	mcs.IDMap.Range(func(key any, value any) bool {
+		mcs.Test.Error("IDMap is not empty when MockCodec closing", key)
+		return false
+	})
+}
+
+func MockTCPServer(t *testing.T, lis net.Listener, doneChan chan struct{}) {
+	rpcSrv, err := obkvrpc.NewServer(RoutinePoolSize, new(chan struct{}))
+	assert.Equal(t, nil, err)
+	wg := new(sync.WaitGroup)
+	for {
+		conn, err := lis.Accept()
+		if err != nil {
+			fmt.Println("listener closed")
+			break
+		}
+		codec := MockCodec{Conn: conn, RunTime: 0, Wg: wg, Test: t, CloseChan: make(chan struct{})}
+		wg.Add(1)
+		go rpcSrv.ServeCodec(&codec)
+	}
+	wg.Wait()
+	close(doneChan)
+	rpcSrv.Close()
+}
+
+func Dial(t *testing.T) {
+	_, err := net.Dial("tcp", "127.0.0.1:1234")
+	assert.Equal(t, nil, err)
+}
+
+func ServerAndClients(t *testing.T, maxMsgNum int, maxClientNum int) {
+	// fmt.Println("start to test tcp server")
+	MaxMsgNum = maxMsgNum
+	lis, err := net.Listen("tcp", "127.0.0.1:1234")
+	assert.Equal(t, nil, err)
+	doneChan := make(chan struct{})
+	go MockTCPServer(t, lis, doneChan)
+	for runTime := 0; runTime < maxClientNum; runTime++ {
+		Dial(t)
+	}
+	lis.Close()
+	_, ok := <-doneChan
+	fmt.Println("finish test tcp server, isOpen = ", ok)
+}
+
+func TestRPCServer(t *testing.T) {
+	// defer goleak.VerifyNone(t)
+	// single message single client
+	ServerAndClients(t, 1, 1)
+	time.Sleep(time.Millisecond * 500)
+
+	// single message multi client
+	ServerAndClients(t, 1, 10000)
+	time.Sleep(time.Millisecond * 500)
+
+	// multi message single client
+	ServerAndClients(t, 10000, 1)
+	time.Sleep(time.Millisecond * 500)
+
+	// multi message multi client
+	ServerAndClients(t, 39000, 256)
+	time.Sleep(time.Millisecond)
+}
