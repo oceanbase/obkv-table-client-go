f165ebf382658038706acbcb89f2ec5528102f45
groundwu <1175416256@qq.com>
2024-06-12 17:43:56 +0800

    fix: table entry do not refresh when create connection fail

--- route/route_info.go
+++ route/route_info.go
@@ -61,6 +61,9 @@ func (i *ObRouteInfo) getTableWithRetry(ctx context.Context, server *ObServerAdd
 		for {
 			select {
 			case <-ctx.Done():
+				err := i.refreshTableLocations(&server.tcpAddr)
+				log.Warn("[Runtime]", nil, "get table fail and try to refresh table location ",
+					log.String("server", server.String()), log.String("err", err.Error()))
 				return nil, errors.Errorf("get table, server:%s", server.String())
 			default:
 				t, ok = i.tableRoster.Get(server.tcpAddr)
